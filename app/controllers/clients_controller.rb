dffec175c5701d99b3258f5c33f7d66a4397807d
Janelle Ballack <janelle@janelleballack.com>
2018-06-06 10:10:03 -0400

    updated calendar

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,55 @@
+class ClientsController < ApplicationController
+    before_action :set_client, only: [:show, :edit, :update, :destroy]
+    
+  def index
+    @clients = Client.all
+ end
+ 
+ def show
+ end
+ 
+ def new 
+    @client = Client.new
+end
+
+def create
+    @client = Client.create(client_params)
+
+    if @client.save
+        redirect_to root_path
+    else
+        render 'new'
+    end
+end
+
+  
+ def edit
+ end
+
+ def update
+    if @client.update(client_params)
+        redirect_to root_path
+    else
+        render 'edit'
+    end
+end
+
+ 
+
+ def destroy
+  @client.destroy
+     redirect_to '/client'
+ end
+
+ 
+ 
+ private
+
+def client_params
+ params.require(:client).permit(:first_name, :last_name, :address, :city, :state, :zipcode, :email, :company, :phone, :project_id)
+end
+
+def set_client
+    @client = Client.find(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_045147) do
+ActiveRecord::Schema.define(version: 2018_06_06_023636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,11 @@ ActiveRecord::Schema.define(version: 2018_06_05_045147) do
     t.integer "client_id"
     t.string "name"
     t.text "description"
-    t.integer "hours"
+    t.string "hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline"
+    t.datetime "completed_at"
   end
 
   create_table "schedules", force: :cascade do |t|
